e0ef1fb09a3ab4ffe3bd911b78f0ba38d239fba2
lesley tabi <esanglesley@gmail.com>
2025-08-30 00:16:18 +0100

    added the enroolment

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -12,7 +12,8 @@ class EnrollmentController extends Controller
      */
     public function index()
     {
-        //
+        $enrollments = Enrollment::all();
+        return view('enrollment.index', compact('enrollments'));
     }
 
     /**
@@ -20,7 +21,7 @@ class EnrollmentController extends Controller
      */
     public function create()
     {
-        //
+        return view('enrollment.create');
     }
 
     /**
@@ -28,7 +29,16 @@ class EnrollmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'enrollment number' => 'required|string|unique:enrollments',
+            'enroll$enrollment_id' => 'required|exists:enroll$enrollments,id',
+            'batch_id' => 'required|exists:batches,id',
+            'enrollment_date' => 'required|date',
+        ]);
+
+        Enrollment::create($validated);
+
+        return redirect()->route('enrollment.index')->with('success', 'Enrollment created successfully');
     }
 
     /**
@@ -36,23 +46,33 @@ class EnrollmentController extends Controller
      */
     public function show(Enrollment $enrollment)
     {
-        //
+        return view('enrollment.show', compact('enrollment'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Enrollment $enrollment)
+    public function edit($id)
     {
-        //
+        $enrollment = enrollment::findOrFail($id);
+        return view('enrollment.edit', compact('enrollment'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Enrollment $enrollment)
+    public function update(Request $request, $id)
     {
-        //
+        $validated = $request->validate([
+            'name'   => 'required|string|max:255',
+            'mobile' => 'required|string|unique:enrollments,mobile,' . $id,
+            'address'=> 'required|string',
+        ]);
+
+        $enrollment = enrollment::findOrFail($id);
+        $enrollment->update($validated);
+
+        return redirect()->route('enrollment.index')->with('success', 'enrollment updated successfully');
     }
 
     /**
@@ -60,6 +80,7 @@ class EnrollmentController extends Controller
      */
     public function destroy(Enrollment $enrollment)
     {
-        //
+        $enrollment->delete();
+        return redirect()->route('enrollment.index')->with('success', 'Enrollment deleted successfully');
     }
 }
